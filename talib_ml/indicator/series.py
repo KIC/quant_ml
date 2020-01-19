536d7bf8d09d541953944c59383673b2b4b6a3f2
kic <ch9.ki7@gmail.com>
2020-01-19 16:21:35 +0100

    dix atr

--- talib_ml/indicator/series.py
+++ talib_ml/indicator/series.py
@@ -13,18 +13,18 @@ def ta_ema(df: PANDAS, period=12):
     return df.ewm(span=period, adjust=False, min_periods=period-1).mean()
 
 
-def ta_macd(df: PANDAS, fast_period=12, slow_period=26, signal_period=9):
+def ta_macd(df: PANDAS, fast_period=12, slow_period=26, signal_period=9, relative=True):
     fast = ta_ema(df, fast_period)
     slow = ta_ema(df, slow_period)
-    macd = (fast - slow)
+    macd = (fast / slow - 1) if relative else (fast - slow)
     signal = ta_ema(macd, signal_period)
     hist = macd - signal
 
-    for l, f in {"macd": macd, "signal": signal, "histogram": hist}.items():
-        if isinstance(f, _pd.DataFrame) and len(df.columns) > 1:
-            f.columns = _pd.MultiIndex.from_product([f.columns, [l]])
+    for label, frame in {"macd": macd, "signal": signal, "histogram": hist}.items():
+        if isinstance(frame, _pd.DataFrame) and len(df.columns) > 1:
+            frame.columns = _pd.MultiIndex.from_product([frame.columns, [label]])
         else:
-            f.name = l
+            frame.name = label
 
     macd = macd.to_frame() if isinstance(macd, _pd.Series) else macd
     return macd.join(signal).join(hist)
@@ -37,9 +37,43 @@ def ta_mom(df: PANDAS, period=10):
 def ta_roc(df: PANDAS, period=10):
     return df.pct_change(period)
 
+
+def ta_apo(df: PANDAS, fast_period=12, slow_period=26, exponential=False):
+    fast = ta_ema(df, fast_period) if exponential else ta_sma(df, fast_period)
+    slow = ta_ema(df, slow_period) if exponential else ta_sma(df, slow_period)
+    return fast - slow
+
+
+def ta_trix(df: PANDAS, period=30):
+    return ta_ema(ta_ema(ta_ema(df, period), period), period).pct_change() * 100
+
+
+def ta_tr(df: PANDAS, high="High", low="Low", close="Close", relative=True):
+    h = df[high]
+    l = df[low]
+    c = df[close].shift(1)
+
+    if relative:
+        ranges = (h / l - 1).rename("a").to_frame() \
+            .join((h / c - 1).rename("b").abs()) \
+            .join((l / c - 1).rename("c").abs())
+    else:
+        ranges = (h - l).rename("a").to_frame()\
+            .join((h - c).rename("b").abs())\
+            .join((l - c).rename("c").abs())
+
+    return ranges.max(axis=1).rename("true_range")
+
+
+def ta_atr(df: PANDAS, period=14, high="High", low="Low", close="Close", relative=True, exponential='wilder'):
+    if exponential is True:
+        return ta_ema(ta_tr(df, high, low, close, relative), period)
+    if exponential == 'wilder':
+        return ta_ema(ta_tr(df, high, low, close, relative), period * 2 - 1)
+    else:
+        return ta_sma(ta_tr(df, high, low, close, relative), period)
+
+# +/- DMI https://www.investopedia.com/terms/d/dmi.asp
 #                                plus_di         = lambda df: talib.PLUS_DI(df["High"], df["Low"], df["Close"]) / 100,
-#                                apo             = lambda df: talib.APO(df["Close"]) / 10,
-#                                atr             = lambda df: talib.ATR(df["High"], df["Low"], df["Close"]) / 10,
 #                                minus_dm        = lambda df: talib.MINUS_DM(df["High"], df["Low"]) / 100,
-#                                trix            = lambda df: talib.TRIX(df["Close"]),
 #                                plus_dm         = lambda df: talib.PLUS_DM(df["High"], df["Low"]) / 100,
--- test/indicator/test__series.py
+++ test/indicator/test__series.py
@@ -17,20 +17,38 @@ class TestSeriesIndicator(TestCase):
         np.testing.assert_array_almost_equal(me, ta)
 
     def test__macd(self):
-        my_macd = ta_macd(DF_TEST["Close"])
+        my_macd = ta_macd(DF_TEST["Close"], relative=False)
         talib_macd = pd.DataFrame(talib.MACD(DF_TEST["Close"])).T
 
         np.testing.assert_array_almost_equal(talib_macd[-100:].values[0], my_macd[-100:].values[0])
-        
+
     def test__mom(self):
-        me = ta_mom(DF_TEST["Close"], 2)[-100:]
-        ta = talib.MOM(DF_TEST["Close"], 2)[-100:]
+        me = ta_mom(DF_TEST["Close"])[-100:]
+        ta = talib.MOM(DF_TEST["Close"])[-100:]
 
         np.testing.assert_array_almost_equal(me, ta)
 
-    def test__mom(self):
-        me = ta_roc(DF_TEST["Close"], 2)[-100:]
-        ta = talib.ROC(DF_TEST["Close"], 2)[-100:] / 100.
+    def test__apo(self):
+        me = ta_apo(DF_TEST["Close"])[-100:]
+        ta = talib.APO(DF_TEST["Close"])[-100:]
+
+        np.testing.assert_array_almost_equal(me, ta)
+
+    def test__trix(self):
+        me = ta_trix(DF_TEST["Close"])[-100:]
+        ta = talib.TRIX(DF_TEST["Close"])[-100:]
+
+        np.testing.assert_array_almost_equal(me, ta)
+
+    def test__tr(self):
+        me = ta_tr(DF_TEST, relative=False)[-100:]
+        ta = talib.TRANGE(DF_TEST["High"], DF_TEST["Low"], DF_TEST["Close"])[-100:]
+
+        np.testing.assert_array_almost_equal(me, ta)
+
+    def test__atr(self):
+        me = ta_atr(DF_TEST, relative=False)[-100:]
+        ta = talib.ATR(DF_TEST["High"], DF_TEST["Low"], DF_TEST["Close"])[-100:]
 
         np.testing.assert_array_almost_equal(me, ta)
 
