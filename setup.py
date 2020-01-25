58c0cdc0c9b70ff096302cb2084c38d38c129d39
kic <ch9.ki7@gmail.com>
2020-01-25 16:33:36 +0100

    add dev requirements

--- setup.py
+++ setup.py
@@ -7,9 +7,12 @@ setup(
    author_email='',
    packages=['quant_ml'],
    scripts=[],
-   url='',
-   license='',
+   url='https://github.com/KIC/quant_ml',
+   license='MIT',
    description='re-implementation of TA-Lib suitable to pandas-ml-utils',
-   long_description='', # open('README.txt').read(),
+   long_description=open('README.txt').read(),
    install_requires=open("requirements.txt").read().splitlines(),
+   extras_require={
+      "dev": open("dev-requirements.txt").read().splitlines(),
+   },
 )
