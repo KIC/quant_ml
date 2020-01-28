70cf88f867162d6ef71e46881bfe849e91dd2520
kic <ch9.ki7@gmail.com>
2020-01-28 10:48:06 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
    url='https://github.com/KIC/quant_ml',
    license='MIT',
    description='re-implementation of TA-Lib suitable to pandas-ml-utils',
-   long_description=open('README.txt').read(),
+   long_description=open('Readme.md').read(),
    install_requires=open("requirements.txt").read().splitlines(),
    extras_require={
       "dev": open("dev-requirements.txt").read().splitlines(),
